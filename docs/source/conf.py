0c3b7123f82a12bf7cb6be7abefd2c8fcce53b52
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2024-03-31 22:28:09 +0200

    move files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Kyrre Sjobak & Erik Adli'
 #Find the project
 import os
 import sys
-sys.path.insert(0,os.path.abspath('../..'))
+sys.path.insert(0,os.path.abspath('../../FYS4565_codelibrary/'))
 #import beamGeneratorLibrary
 
 # -- General configuration ---------------------------------------------------
